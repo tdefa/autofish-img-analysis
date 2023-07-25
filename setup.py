42bf356ab69bf8c5835106ff183aa40a3de8173e
tdefa <thomas.defard@minesparistech.fr>
2023-07-25 09:52:46 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='autofish',
+      version='0.1',
+      
+    url='https://github.com/tdefa/autofish_pckg',
+      description='The funniest joke in the world',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+
+
+      zip_safe=False)
