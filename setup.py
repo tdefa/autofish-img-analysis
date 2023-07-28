ed8cd65764ac79b9fa5980964843920cbb68ec48
tdefa <thomas.defard@minesparistech.fr>
2023-07-28 13:58:27 +0200

    rename

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-from setuptools import setup
-
-setup(name='autofish',
-      version='0.1',
-      
-    url='https://github.com/tdefa/autofish_pckg',
-      description='The funniest joke in the world',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
-      license='MIT',
-
-
-      zip_safe=False)
+import setuptools 
+if __name__ == "__main__": 
+   setuptools.setup( packages=['autofish'])
